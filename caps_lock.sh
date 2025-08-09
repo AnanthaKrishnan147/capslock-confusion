f88f75c1329be1bfb88da7773e997f6700d9de6f
AnanthaKrishnan147 <ananthakrishnan.kousthubam@gmail.com>
2025-08-09 14:55:46 +0530

    This code is a shell script which is to be run on linux

--- caps_lock.sh
+++ caps_lock.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Number of times to toggle Caps Lock
-TOGGLE_COUNT=9999999999999999999999999999999999999
-
-for ((i=1; i<=TOGGLE_COUNT; i++))
+while true
 do
-  # Toggle Caps Lock key
   xdotool key Caps_Lock
- 
-  # Wait for a random time between 0.2 to 1.5 seconds
-  sleep_time=$(awk -v min=0.2 -v max=1.5 'BEGIN{srand(); print min+rand()*(max-min)}')
-  sleep $sleep_time
+  sleep 0.5  # wait half a second before toggling again
 done
+
 
 
 
